cdd945a3b4e343a43870809cd0773e9fe0df9946
laurita-BIT <lauragranese15@gmail.com>
2026-06-01 16:24:33 +0200

    Update vehiclemaintenance.h

--- Model/vehiclemaintenance.h
+++ Model/vehiclemaintenance.h
@@ -34,7 +34,7 @@ public:
 
     // Override per la persistenza dati
     QJsonObject toJsonObject() const override;
-    void fromJsonObject(const QJsonObject& json) override;
+   Abstract_Activity* cloneFromJson(const QJsonObject& json) const override;
     void writeToXml(QXmlStreamWriter& writer) const override;
 };
 
