c91cf5bf09d6fd1c4e0fe3c5e5084e64936ae13b
laurita-BIT <lauragranese15@gmail.com>
2026-06-01 16:11:06 +0200

    Update abstract_activity.cpp

--- Model/abstract_activity.cpp
+++ Model/abstract_activity.cpp
@@ -20,38 +20,7 @@ void Abstract_Activity::setDescrizione(const QString &descrizione) { this->descr
 void Abstract_Activity::setCompletata(bool stato) { this->completata = stato; }
 void Abstract_Activity::completaAttivita() { this->completata = true; }
 
-//Persistenza
+// NOTA: I metodi 'toJsonObject()', 'cloneFromJson()' e 'writeToXml()' 
+// sono virtuali puri (= 0) nell'header, quindi NON devono essere implementati qui.
 
-QJsonObject Abstract_Activity::toJsonObject() const {
-    QJsonObject json;
-    json["tipo"] = getTipoClasse();
-    json["id"] = id;
-    json["titolo"] = titolo;
-    json["descrizione"] = descrizione;
-    json["completata"] = completata;
-    return json;
-}
 
-// Factory di Caricamento
-Abstract_Activity* Abstract_Activity::fromJson(const QJsonObject &json) {
-    if (!json.contains("tipo") || !json["tipo"].isString()) {
-        return nullptr;
-    }
-
-    QString type = json["tipo"].toString();
-
-    // Delega la creazione e il riempimento dei dati specifici alle classi figlie
-    if (type == "HomeTask") {
-        return HomeTask::fromJson(json);
-    } else if (type == "Bill") {
-        return Bill::fromJson(json);
-    } else if (type == "VehicleMaintenance") {
-        return VehicleMaintenance::fromJson(json);
-    } else if (type == "ShoppingTask") {
-        return ShoppingTask::fromJson(json);
-    } else if (type == "LeisureTimeTask") {
-        return LeisureTimeTask::fromJson(json); 
-    }
-
-    return nullptr;
-}
