c7fb624f3c4a598c19ca6cc79c378cfa77d8c30e
laurita-BIT <lauragranese15@gmail.com>
2026-06-01 16:05:05 +0200

    Update abstract_activity.cpp

--- Model/abstract_activity.cpp
+++ Model/abstract_activity.cpp
@@ -8,41 +8,23 @@
 Abstract_Activity::Abstract_Activity(const QString &id, const QString &titolo, const QString &descrizione, bool completata)
     : id(id), titolo(titolo), descrizione(descrizione), completata(completata) {
 }
+// Getters
+QString Abstract_Activity::getId() const { return id; }
+QString Abstract_Activity::getTitolo() const { return titolo; }
+QString Abstract_Activity::getDescrizione() const { return descrizione; }
+bool Abstract_Activity::isCompletata() const { return completata; }
 
-QString Abstract_Activity::getId() const {
-    return id;
-}
+// Setters
+void Abstract_Activity::setTitolo(const QString &titolo) { this->titolo = titolo; }
+void Abstract_Activity::setDescrizione(const QString &descrizione) { this->descrizione = descrizione; }
+void Abstract_Activity::setCompletata(bool stato) { this->completata = stato; }
+void Abstract_Activity::completaAttivita() { this->completata = true; }
 
-QString Abstract_Activity::getTitolo() const {
-    return titolo;
-}
-
-QString Abstract_Activity::getDescrizione() const {
-    return descrizione;
-}
-
-bool Abstract_Activity::isCompletata() const {
-    return completata;
-}
-
-void Abstract_Activity::setTitolo(const QString &titolo) {
-    this->titolo = titolo;
-}
-
-void Abstract_Activity::setDescrizione(const QString &descrizione) {
-    this->descrizione = descrizione;
-}
-
-void Abstract_Activity::setCompletata(bool stato) {
-    this->completata = stato;
-}
-
-void Abstract_Activity::completaAttivita() {
-    this->completata = true;
-}
+//Persistenza
 
 QJsonObject Abstract_Activity::toJsonObject() const {
     QJsonObject json;
+    json["tipo"] = getTipoClasse();
     json["id"] = id;
     json["titolo"] = titolo;
     json["descrizione"] = descrizione;
@@ -50,24 +32,15 @@ QJsonObject Abstract_Activity::toJsonObject() const {
     return json;
 }
 
-void Abstract_Activity::fromJsonObject(const QJsonObject &json) {
-    if (json.contains("id") && json["id"].isString()) {
-        id = json["id"].toString();
-    }
-    if (json.contains("titolo") && json["titolo"].isString()) {
-        titolo = json["titolo"].toString();
-    }
-    if (json.contains("descrizione") && json["descrizione"].isString()) {
-        descrizione = json["descrizione"].toString();
-    }
-    if (json.contains("completata") && json["completata"].isBool()) {
-        completata = json["completata"].toBool();
-    }
-}
-
+// Factory di Caricamento
 Abstract_Activity* Abstract_Activity::fromJson(const QJsonObject &json) {
+    if (!json.contains("tipo") || !json["tipo"].isString()) {
+        return nullptr;
+    }
+
     QString type = json["tipo"].toString();
 
+    // Delega la creazione e il riempimento dei dati specifici alle classi figlie
     if (type == "HomeTask") {
         return HomeTask::fromJson(json);
     } else if (type == "Bill") {
@@ -77,7 +50,7 @@ Abstract_Activity* Abstract_Activity::fromJson(const QJsonObject &json) {
     } else if (type == "ShoppingTask") {
         return ShoppingTask::fromJson(json);
     } else if (type == "LeisureTimeTask") {
-        return LeisureTimeTask::fromJson(json); // Allineato al nome file o classe corretto
+        return LeisureTimeTask::fromJson(json); 
     }
 
     return nullptr;
