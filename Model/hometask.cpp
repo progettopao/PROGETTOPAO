b9445fff6616b869c17a8148fb344c2bb04878a0
laurita-BIT <lauragranese15@gmail.com>
2026-06-04 10:59:23 +0200

    Update hometask.cpp

--- Model/hometask.cpp
+++ Model/hometask.cpp
@@ -31,7 +31,7 @@ bool HomeTask::isUrgente() const {
 
 QJsonObject HomeTask::toJsonObject() const {
     QJsonObject json;
-    json["att_type"] = "HomeTask"; // Uniformato con le altre classi per il PersistenceManager
+    json["att_type"] = "HomeTask";
     json["id"] = getId();
     json["titolo"] = getTitolo();
     json["descrizione"] = getDescrizione();
